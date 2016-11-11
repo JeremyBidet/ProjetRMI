60e621641dfdf1169a0af59872cd8945cee3c9e6
Jeremy Bidet <jeremy.bidet@gmail.com>
2016-11-11 01:45:49 +0100

    update users

--- src/Server.java
+++ src/Server.java
@@ -9,6 +9,13 @@ public class Server {
 			
 			//TODO: add some vehicles with comments and user to test
 			
+			//admin@rmi.net pass@word1
+			//jbidet@etud.u-pem.fr pass@word1
+			//eramos@etud.u-pem.fr pass@word2
+			//rforax@igm.u-pem.fr pass@word3
+			//jramsa@etud.u-pem.fr pass@word4
+			//mpbeal@igm.u-pem.fr pass@word5
+			
 			Naming.rebind("rmi://localhost/AuthenticationService", auth);
 			Naming.rebind("rmi://localhost/ParkService", park);
 		} catch(Exception e) {
