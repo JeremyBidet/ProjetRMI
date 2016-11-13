2c854d93154d83ab3eb05e70b6193cce4c354e40
enzoramos <enzo-ramos@live.com>
2016-11-13 21:52:12 +0100

    update gui

---
+++ src/ClientFx.java
@@ -0,0 +1,14 @@
+import java.rmi.Naming;
+import java.rmi.RMISecurityManager;
+
+public class ClientFx {
+
+	try {
+		String codebase = "file:///home/whyt/workspace/ProjetRMI/src/";
+		System.setProperty("java.rmi.server.codebase", codebase);
+		System.setProperty("java.security.policy", "/home/whyt/workspace/ProjetRMI/resources/all.policy");
+		System.setSecurityManager(new RMISecurityManager());
+		
+		IAuthentication auth = (IAuthentication) Naming.lookup("rmi://localhost/AuthenticationService");
+	
+}
